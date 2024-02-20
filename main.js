36ce871007127ff33bad68a348437a8003c1eb07
バナナフライ <153720938+YasaiRa-men@users.noreply.github.com>
2024-02-20 11:45:05 +0900

    Update main.js

--- main.js
+++ main.js
@@ -11,3 +11,10 @@ document.addEventListener("mousemove", function(e) {
     })
 
 });
+
+function disableScroll(event) {
+  event.preventDefault();
+}
+
+// イベントと関数を紐付け
+document.addEventListener('touchmove', disableScroll, { passive: false });
